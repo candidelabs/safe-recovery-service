cfe69debb74d46df3763b6015951332a46e70c71
Sherif Abdelmoatty <sherif.ahmed990@gmail.com>
2025-04-05 09:43:31 +0200

    add: auth tests

--- test/auth.test.ts
+++ test/auth.test.ts
@@ -1,8 +1,110 @@
 import supertest from 'supertest';
 import {app} from '../src/index';
+import {SiweMessage} from "siwe";
+import {ethers, hashMessage} from "ethers6";
+
+function getMessageHashForSafe(
+    accountAddress: string, payload: string, chainId: number
+){
+    const SAFE_MSG_TYPEHASH = "0x60b3cbf8b4a223d68d641b3b6ddf9a298e7f33710cf3d3a9d1146b5a6150fbca";
+    const DOMAIN_SEPARATOR_TYPEHASH = "0x47e79534a245952e8b16893a336b85a3d9ea9fa8c573f3d803afb92a79469218";
+    const domainSeparator = ethers.keccak256(ethers.AbiCoder.defaultAbiCoder().encode(
+      ["bytes32", "uint256", "address"],
+      [DOMAIN_SEPARATOR_TYPEHASH, chainId, accountAddress]
+    ));
+    const encodedMessage = ethers.AbiCoder.defaultAbiCoder().encode(
+      ["bytes32", "bytes32"],
+      [SAFE_MSG_TYPEHASH, ethers.keccak256(payload)]
+    );
+    const messageHash = ethers.keccak256(ethers.solidityPacked(
+      ["bytes1", "bytes1", "bytes32", "bytes32",],
+      [Uint8Array.from([0x19]), Uint8Array.from([0x01]), domainSeparator, ethers.keccak256(encodedMessage)]
+    ));
+    return messageHash;
+}
+
+function personalSign(
+    accountAddress: string, payload: string, chainId: number, privateKey: string
+){
+    payload = hashMessage(payload);
+    const messageHash = getMessageHashForSafe(accountAddress, payload, chainId);
+    const signer = new ethers.Wallet(privateKey);
+    return signer.signingKey.sign(messageHash).serialized;
+}
 
 describe('auth', ()=>{
     it('should return a 404 if wrong path', async ()=>{
         await supertest(app).get('/v1/auth/wrong/').expect(404);
+    });
+    describe('auth', ()=>{
+        it('should fail with 400 to register if wrong message format', async ()=>{
+            await supertest(app).post('/v1/auth/register/')
+            .send({
+                "account": "0x0000000000000000000000000000000000000000",
+                "chainId": 11155111,
+                "channel":"email",
+                "target":"user@example.com",
+                "message":{
+                  statement: "invalid format",
+                },
+                "signature": "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef"
+            })
+            .expect(400).then((response) => {
+                expect(response.body.message).toContain(
+                    "failed custom validation"
+                )
+            });
+        });
+        
+        it('should fail with 400 to register if wrong message', async ()=>{
+            await supertest(app).post('/v1/auth/register/')
+            .send({
+                "account": "0x0000000000000000000000000000000000000000",
+                "chainId": 11155111,
+                "channel":"email",
+                "target":"user@example.com",
+                "message":{
+                  version: "1",
+                  address: "0x0000000000000000000000000000000000000000",
+                  domain: "service://safe-recovery-service",
+                  uri: "service://safe-recovery-service",
+                  //statement: "I request to retrieve all authentication methods currently registered to my account with Safe Recovery Service",
+                  statement: "invalid message",
+                  chainId: 11155111,
+                  nonce: 0, 
+                },
+                "signature": "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef"
+            })
+            .expect(400).then((response) => {
+                expect(response.body.message).toContain(
+                    "invalid message"
+                )
+            });
+        });
+
+        it('should fail with 403 to register if invalid signature', async ()=>{
+            await supertest(app).post('/v1/auth/register/')
+            .send({
+                "account": "0x0000000000000000000000000000000000000000",
+                "chainId": 11155111,
+                "channel":"email",
+                "target":"user@example.com",
+                "message":{
+                  version: "1",
+                  address: "0x0000000000000000000000000000000000000000",
+                  domain: "service://safe-recovery-service",
+                  uri: "service://safe-recovery-service",
+                  statement: "I authorize Safe Recovery Service to sign a recovery request for my account after I authenticate using user@example.com (via email)",
+                  chainId: 11155111,
+                  nonce: 0, 
+                },
+                "signature": "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef"
+            })
+            .expect(403).then((response) => {
+                expect(response.body.message).toContain(
+                    "invalid signature"
+                )
+            });
+        });
     });
 });
