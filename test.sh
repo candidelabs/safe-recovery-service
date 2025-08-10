0f7bdbfae7586053833bde1059aaaf5ac9f87866
Sherif Abdelmoatty <sherif.ahmed990@gmail.com>
2025-08-10 15:23:40 +0300

    add alerts test

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
+docker stop integration-tests-prisma mailpit
+docker rm integration-tests-prisma mailpit
 docker compose -f test/docker-compose.yml up -d \
     && sleep 1 \
     && bunx prisma migrate deploy \
-    && bun test "$1"\
-    && docker compose -f test/docker-compose.yml down || docker compose -f test/docker-compose.yml down
+    && bun test --timeout 100000 "$1"
+docker stop integration-tests-prisma mailpit
+docker rm integration-tests-prisma mailpit
