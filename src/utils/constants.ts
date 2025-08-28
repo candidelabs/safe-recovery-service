4db4b4908d79e65891338997f0c5abd812f45f8d
Sherif Abdelmoatty <sherif.ahmed990@gmail.com>
2025-08-28 23:02:15 +0300

    refactor: modify alerts-subscribe SIWE message to include account address

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -12,7 +12,7 @@ export const MessageStatements = {
   "auth-fetch": "I request to retrieve all authentication methods currently registered to my account with Safe Recovery Service",
   "auth-delete": "I request to remove the authentication method with registration ID {{id}} from my account on Safe Recovery Service",
   //
-  "alerts-subscribe": "I agree to receive Social Recovery Module alert notifications for my account address on all supported chains sent to {{target}} (via {{channel}})",
+  "alerts-subscribe": "I agree to receive Social Recovery Module alert notifications for {{account}} on all supported chains sent to {{target}} (via {{channel}})",
   "alerts-fetch": "I request to retrieve all Social Recovery Module alert subscriptions linked to my account",
   "alerts-unsubscribe": "I request to unsubscribe from all Social Recovery Module alert subscriptions linked to my account",
 }
